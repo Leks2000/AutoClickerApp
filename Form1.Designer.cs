89e99452e229c18272ad2401d1cf372e5ca36961
Александр <107043945+Leks2000@users.noreply.github.com>
2025-10-19 15:20:32 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace AutoClickerApp
+{
+    partial class AutoClickerApp
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.TextInterval = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnStart.Font = new System.Drawing.Font("Yu Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStart.ForeColor = System.Drawing.Color.Black;
+            this.btnStart.Location = new System.Drawing.Point(41, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(208, 103);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnStop.Font = new System.Drawing.Font("Yu Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStop.ForeColor = System.Drawing.Color.Black;
+            this.btnStop.Location = new System.Drawing.Point(41, 111);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(208, 114);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Font = new System.Drawing.Font("Yu Gothic UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtInterval.Location = new System.Drawing.Point(108, 236);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(173, 25);
+            this.txtInterval.TabIndex = 2;
+            // 
+            // TextInterval
+            // 
+            this.TextInterval.Font = new System.Drawing.Font("Yu Gothic UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextInterval.Location = new System.Drawing.Point(3, 239);
+            this.TextInterval.Name = "TextInterval";
+            this.TextInterval.Size = new System.Drawing.Size(100, 23);
+            this.TextInterval.TabIndex = 4;
+            this.TextInterval.Text = "Интервал (мс):";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "F6 - пауза - продолжение";
+            // 
+            // AutoClickerApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 293);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextInterval);
+            this.Controls.Add(this.txtInterval);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AutoClickerApp";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoClickerApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.Label TextInterval;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoClickerApp
+{
+    public partial class AutoClickerApp : Form
+    {
+        public AutoClickerApp()
+        {
+            InitializeComponent();
+
+            RegisterHotKey(this.Handle, HOTKEY_ID, 0, VK_F6);
+        }
+
+
+        [DllImport("user32.dll")] // Убрали лишние CharSet и CallingConvention
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, UIntPtr dwExtraInfo);
+        // Импорт функций для работы с глобальными горячими клавишами
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        // Константы для событий мыши
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        // Идентификатор горячей клавиши (любое уникальное число)
+        private const int HOTKEY_ID = 9000;
+        // Код клавиши F6
+        private const int VK_F6 = 0x75;
+
+        // ----------------------------------------------------
+        // 2. Логика Автокликера
+        // ----------------------------------------------------
+
+        private bool isClicking = false;
+        private Thread clickerThread;
+        private int clickInterval = 100; // Значение по умолчанию 100 мс
+
+        // Метод симуляции левого клика
+        private void SimulateLeftClick()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, UIntPtr.Zero);
+            Thread.Sleep(10); // Краткая задержка между нажатием и отпусканием
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, UIntPtr.Zero);
+        }
+
+        // Метод, который будет выполняться в отдельном потоке
+        private void ClickerLoop()
+        {
+            while (isClicking)
+            {
+                SimulateLeftClick();
+                Thread.Sleep(clickInterval);
+            }
+        }
+
+        private void StartClicker()
+        {
+            if (isClicking) return;
+
+            // Считывание интервала и проверка корректности
+            if (int.TryParse(txtInterval.Text, out int interval) && interval >= 1)
+            {
+                clickInterval = interval;
+            }
+            else
+            {
+                MessageBox.Show("Некорректный интервал. Установите положительное число.");
+                return;
+            }
+
+            isClicking = true;
+            clickerThread = new Thread(ClickerLoop);
+            clickerThread.IsBackground = true; // Поток завершится при закрытии приложения
+            clickerThread.Start();
+
+            // Обновление UI (нужно использовать Invoke, так как мы в другом потоке)
+            this.Invoke((MethodInvoker)delegate {
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+                this.Text = "AutoClicker (Активен)";
+            });
+        }
+
+        private void StopClicker()
+        {
+            isClicking = false;
+
+            // Обновление UI
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            this.Text = "AutoClicker (Остановлен)";
+        }
+
+        // ----------------------------------------------------
+        // 3. Обработчики событий UI и Горячих клавиш
+        // ----------------------------------------------------
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartClicker();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopClicker();
+        }
+
+        // Перехват системного сообщения о нажатии горячей клавиши
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            if (m.Msg == WM_HOTKEY && (int)m.WParam == HOTKEY_ID)
+            {
+                // Нажата F6
+                if (isClicking)
+                {
+                    StopClicker();
+                }
+                else
+                {
+                    StartClicker();
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        // Освобождение горячей клавиши при закрытии формы
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Остановка кликера и отмена регистрации горячей клавиши
+            isClicking = false;
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+        }
+    }
+}
